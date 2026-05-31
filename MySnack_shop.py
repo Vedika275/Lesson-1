ff921016d9b87bfc5e8565f6d1e0a25ca84eedc4
Vedika275 <vedikakapur4@gmail.com>
2026-05-31 14:50:40 +0530

    commit

---
+++ MySnack_shop.py
@@ -0,0 +1,36 @@
+snack_name  =  "Chips"
+price  = 1.50
+quantity = 10
+is_available = True
+print("Snack Name:", snack_name)
+print("Price:", price)
+print("Quantity:", quantity)
+print("Is Available:", is_available)
+
+print (type(snack_name))
+print (type(price))
+print (type(quantity))
+print (type(is_available))
+
+total_price = price * quantity
+print("Total Price:", total_price)
+
+
+
+
+print( "is price less than 2?", price <  2)
+print( "is quantity greater than 5?", quantity > 5)
+
+
+
+shop_name = "quick" + " " + "snack" 
+print("Shop Name:", shop_name)
+
+
+a=5
+b=10
+temp = a
+a=b
+b=temp
+print("Value of a:", a)
+print("Value of b:", b)
